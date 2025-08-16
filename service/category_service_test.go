f450e301216938b3fb4ca115cf2e44915e5e55a4
alfianchii <pianaee379@gmail.com>
2025-08-16 19:32:24 +0700

    feat: mock

---
+++ service/category_service_test.go
@@ -0,0 +1,36 @@
+package service
+
+import (
+	"testing"
+
+	"github.com/alfianchii/golang-unit-test/entity"
+	"github.com/alfianchii/golang-unit-test/repository"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+var categoryRepository = &repository.CategoryRepositoryMock{Mock: mock.Mock{}}
+var categoryService = CategoryService{Repository: categoryRepository}
+
+func TestCategoryService_GetNotFound(t *testing.T) {
+	categoryRepository.Mock.On("FindById", "1").Return((*entity.Category)(nil))
+
+	category, err := categoryService.Get("1")
+	assert.Nil(t, category)
+	assert.NotNil(t, err)
+}
+
+func TestCategoryService_GetSuccess(t *testing.T) {
+	category := &entity.Category{
+		Id: "1",
+		Name: "Computer",
+	}
+
+	categoryRepository.Mock.On("FindById", "2").Return(category)
+
+	result, err := categoryService.Get("2")
+	assert.Nil(t, err)
+	assert.NotNil(t, result)
+	assert.Equal(t, category.Id, result.Id)
+	assert.Equal(t, category.Name, result.Name)
+}
